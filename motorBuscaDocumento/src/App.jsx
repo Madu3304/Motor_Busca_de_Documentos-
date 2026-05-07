aa0c93b7517e36f781a37e4965909e9ad88bd994
Madu3304 <mdua0722@gmail.com>
2026-05-06 23:48:29 -0300

    modelo

---
+++ motorBuscaDocumento/src/App.jsx
@@ -0,0 +1,12 @@
+import Scanner from './componentes/Scanner'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Scanner />
+    </div>
+  )
+}
+
+export default App
---
+++ motorBuscaDocumento/src/componentes/Scanner.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+import './Scanner.css'
+
+const Scanner = () => {
+  const [file, setFile] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [results, setResults] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [algorithm, setAlgorithm] = useState('naive')
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0])
+  }
+
+  const handleSearch = async () => {
+    if (!file || !searchTerm) {
+      alert('Selecione um arquivo e digite um termo de busca!')
+      return
+    }
+
+    setLoading(true)
+
+    const formData = new FormData()
+    formData.append('file', file)
+    formData.append('keyword', searchTerm)
+    formData.append('algorithm', algorithm)
+
+    try {
+      const response = await fetch('http://localhost:8000/search', {
+        method: 'POST',
+        body: formData,
+      })
+
+      const data = await response.json()
+      setResults(data.matches || [])
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+  <div className="scannerpai">
+    <h1>Localizador de Termos</h1>
+
+    <div className="botaopesquisa">
+
+      <div title="Formatos aceitos: .txt e .pdf">
+        <input
+          type="file"
+          accept=".pdf,.txt"
+          onChange={handleFileChange}
+        />
+
+        <select
+            value={algorithm}
+            onChange={(e) => setAlgorithm(e.target.value)}
+            className="selectalgoritmo"
+            >
+            <option value="naive">
+                Busca Simples
+            </option>
+
+            <option value="kmp">
+                KMP
+            </option>
+        </select>
+      </div>
+
+      <input
+        type="text"
+        placeholder="Digite a palavra ou frase..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="texto"
+      />
+
+      <button
+        onClick={handleSearch}
+        disabled={loading}
+        className="botaoazul"
+      >
+        {loading ? 'Buscando...' : 'Localizar'}
+      </button>
+    </div>
+
+    <div className="resultado">
+      <h3>Resultados encontrados:</h3>
+
+      {results.length > 0 ? (
+        <ul className="resultadolistado">
+          {results.map((res, index) => (
+            <li key={index} className="item">
+              ...{res}...
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="naolocalizado">
+          Nenhum resultado para exibir.
+        </p>
+      )}
+    </div>
+  </div>
+)
+}
+
+export default Scanner
